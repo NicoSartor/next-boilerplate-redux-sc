e9ecf7abd098c2bd1b6e74270768d3341f8441e9
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-05 22:41:15 -0700

    seems to be working again?

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-const express = require('express')
+// server.js
 const next = require('next')
-const Router = require('./routes')
- 
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const server = express()
-const handle = Router.getRequestHandler(app)
- 
-app.prepare()
-  .then(() => {
-	server.get('/params', (req, res) => { //define the empty one, next can't handle it
-		return app.render(req, res, '/params')
-	})
-    server.get('/params/:id', (req, res) => {
-		return app.render(req, res, '/params', req.params)
-	})
-    server.listen(3000)
-  })
+const routes = require('./routes')
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handler = routes.getRequestHandler(app)
+
+// With express
+const express = require('express')
+app.prepare().then(() => {
+  express().use(handler).listen(3000)
+})

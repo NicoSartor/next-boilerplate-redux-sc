42a87c7c4dbb4e5604a01583ba493aea2d7ffcfd
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-06 10:02:42 -0700

    better param description\

--- pages/params.js
+++ pages/params.js
@@ -20,7 +20,7 @@ class ParamsExample extends React.Component {
 				<p>Adding params require you to start the server.js file instead of running next commands, hot reload breaks</p>
 				<p>On routes.js define your route with params.</p>
 				<p>Dyanmic routing is built with next-routes npm package, and is implemented by adding another server, make sure your portare correct</p>
-				<p><strong>If you want to get rid of dynamic routes</strong>, remove server.js and router.js</p>
+				<p><strong>If you want to get rid of dynamic routes</strong>, remove server.js and router.js and change package.json scripts to default next scripts</p>
 				<p>Try adding a param to the url, it should show here: <span>{this.props.query.id}</span></p>
 				<p>You can add another param, but that needs a different route and page.</p>
 			</div>

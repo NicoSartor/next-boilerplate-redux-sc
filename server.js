425f3809e8aa3085c86276d4f41543d1c39b5752
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-06 09:22:21 -0700

    added server listener log

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // server.js
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
+const next = require("next");
+const routes = require("./routes");
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handler = routes.getRequestHandler(app);
 
 // With express
-const express = require('express')
+const express = require("express");
 app.prepare().then(() => {
-  express().use(handler).listen(3000)
-})
+	express()
+		.use(handler)
+		.listen(3000, (err) => {
+			if (err) {
+				console.log("[SERVER START ERROR]", err);
+			}
+			console.log("👍[LISTENING ON PORT 3000]👍");
+		});
+});

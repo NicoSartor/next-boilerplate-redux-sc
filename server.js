e61b081c0929d6654a574c34e3e06112fad2efda
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-06 10:31:38 -0700

    added port configuration for heroku deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 
-const buildType = ( process.env.NODE_ENV == "production"  )? 'Production' : 'Development';
+const buildType = ( process.env.NODE_ENV == "production"  )? 'Production (If you want hot reloading, use command: npm run dev)' : 'Development';
 const port = process.env.PORT || 3000;
 
 // With express

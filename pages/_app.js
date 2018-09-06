fe6185f52cf693322be6e5e99fee0237e43ce026
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-05 22:07:51 -0700

    links formatting

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import React from "react";
 import withReduxStore from "../lib/with-redux-store";
 import { Provider } from "react-redux";
 //import Link from "next/link";
-import {Link} from '../routes'
+import { Link } from "../routes";
 
 class MyApp extends App {
 	render() {
@@ -11,15 +11,24 @@ class MyApp extends App {
 		return (
 			<Container>
 				<div>
-					<p>This is a global header</p>
-					<Link href="/">
-						<a href="">Index</a>
-					</Link>
-					<span> </span>
-					<Link href="/home">
-						<a href="">Home(Clock)</a>
-					</Link>
-					<Link href={`/params`} params={{id: 'some_param'}}><a>Params Example2</a></Link>
+					<p>This is a global header (but it also skips 404)</p>
+					<ul>
+						<li>
+							<Link href="/">
+								<a href="">Index</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="/home">
+								<a href="">Home(Clock)</a>
+							</Link>
+						</li>
+						<li>
+							<Link href={`/params`} params={{ id: "some_param" }}>
+								<a>Params Example2</a>
+							</Link>
+						</li>
+					</ul>
 				</div>
 
 				<Provider store={reduxStore}>

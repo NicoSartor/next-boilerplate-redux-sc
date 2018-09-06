d4e150d6c4725950b14cedbded0cb55e5a59ffb6
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-06 10:30:24 -0700

    added port configuration for heroku deployment

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 
 const buildType = ( process.env.NODE_ENV == "production"  )? 'Production' : 'Development';
+const port = process.env.PORT || 3000;
 
 // With express
 const express = require("express");
 app.prepare().then(() => {
 	express()
 		.use(handler)
-		.listen(3000, (err) => {
+		.listen(port, (err) => {
 			if (err) {
 				console.log("[SERVER START ERROR]", err);
 			}
-			console.log("[LISTENING ON PORT 3000]👍 ", buildType);
+			console.log(`[LISTENING ON PORT ${port}]👍  live build: ${buildType}` );
 		});
 });

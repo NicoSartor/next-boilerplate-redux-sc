ec50866dcac5db8540922fba437c2fbebae5b116
Nicolas Kao Sartor <nkao552@gmail.com>
2018-09-05 22:51:44 -0700

    added nodemon hotreload

--- pages/params.js
+++ pages/params.js
@@ -17,11 +17,12 @@ class ParamsExample extends React.Component {
 		return (
 			<div>
 				<h1>Params Page</h1>
+				<p>Adding params require you to start the server.js file instead of running next commands, hot reload breaks</p>
 				<p>On routes.js define your route with params.</p>
 				<p>On server.js define the route, also set the route without params, so next doesnt break</p>
-				<p>Still not sure how to set multiple params, ex(/users/:userId/:postId)</p>
 				<p>Test <a href='https://www.npmjs.com/package/nextjs-dynamic-routes'>nextjs-dynamic-routes</a> later on</p>
 				<p>Try adding a param to the url, it should show here: <span>{this.props.query.id}</span></p>
+				<p>You can add another param, but that needs a different route and page.</p>
 			</div>
 		);
 	}
